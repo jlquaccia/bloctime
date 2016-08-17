810cf16a3b59767ff0158b08d2c023340377d226
Jason Quaccia <jlquaccia@gmail.com>
2016-08-17 14:18:12 -0700

    completed checkpoint 4: take a 30 minute break

--- app/scripts/services/Timer.js
+++ app/scripts/services/Timer.js
@@ -1,21 +1,32 @@
 (function() {
   function Timer($interval, TIME_CONSTANTS) {
-    // var BREAKTIME = 300;
-    // var POMODOROTIME = 1500;
-    var isRunning = false;
-    var currentTime = TIME_CONSTANTS.POMODOROTIME;
-    var stopTime;
-    var timerState = "pomodoro";
-    var btnText = "Start";
+    var isRunning = false,
+        currentTime = TIME_CONSTANTS.POMODOROTIME,
+        stopTime,
+        timerState = "pomodoro",
+        btnText = "Start",
+        completedWorkSessions = 0;
 
     function slayer(){
       if(currentTime > 0){
         currentTime = currentTime - 1;
-      }else{
+      } else {
+        
+        console.log(timerState);
+
         if(timerState == "pomodoro"){
-          timerState = "break";
-          currentTime = TIME_CONSTANTS.BREAKTIME;
-        }else{
+          if (completedWorkSessions == 3) {
+            timerState = "thirtyMinuteBreak";
+            currentTime = TIME_CONSTANTS.THIRTY_MINUTE_BREAKTIME;
+            completedWorkSessions = 0;
+            console.log('completed work sessions have been reset');
+          } else {
+            timerState = "break";
+            currentTime = TIME_CONSTANTS.BREAKTIME;
+            completedWorkSessions += 1;
+            console.log(completedWorkSessions + ' completed work sessions');
+          }
+        } else {
           timerState = "pomodoro";
           currentTime = TIME_CONSTANTS.POMODOROTIME;
         }
@@ -53,6 +64,13 @@
             stopTime = null;
             isRunning = false;
             currentTime = TIME_CONSTANTS.POMODOROTIME;
+            btnText = 'Start';
+          } else if (timerState == 'thirtyMinuteBreak') {
+            console.log("RESET");
+            $interval.cancel(stopTime);
+            stopTime = null;
+            isRunning = false;
+            currentTime = TIME_CONSTANTS.THIRTY_MINUTE_BREAKTIME;
             btnText = 'Start';
           } else {
             console.log("RESET");
--- dist/scripts/constants/TIME_CONSTANTS.js
+++ dist/scripts/constants/TIME_CONSTANTS.js
@@ -3,6 +3,7 @@
     .module('bloctime')
     .constant('TIME_CONSTANTS', {
       'BREAKTIME': 300,
-      'POMODOROTIME': 1500
+      'POMODOROTIME': 1500,
+      'THIRTY_MINUTE_BREAKTIME': 1800
     });
 })();
